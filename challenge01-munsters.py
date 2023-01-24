4ce4e4eb66580311282e119090fae18065b7f712
dwinn67 <winnd67@gmail.com>
2023-01-24 18:11:47 +0000

    code challenge with lists and dictionaries

--- challenge01-munsters.py
+++ challenge01-munsters.py
@@ -24,4 +24,19 @@ print( munsters.values())
 print (munsters.get('episodes'))
 
 
+# Break down the data with a "loop"
+# define a new "list" named "names" for the purposes of teaching a simple loop
+names = ['Lily', 'Herman', 'Grandpa', 'Eddie', 'Marilyn']
+
+# create a simple for loop
+for x in names:   # if a line ends in a colon the next line WILL be indented
+    print(x, "is a character on the Munsters")
+    # print("---")  # print 3 dashes on the screen each time through the loop
+
+# we don't have to use a simple list to loop
+# we could access the nested list within the dictionary->   munsters.get('name')
+print("\nDisplays the same data, but by accessing the nested list within our dict\n")
+for x in munsters.get("names"):
+    print(x, "is a character on the Munsters")
+    
 
